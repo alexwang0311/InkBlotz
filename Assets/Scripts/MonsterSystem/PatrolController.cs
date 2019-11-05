acfa15334f4a3d7c4b455aea090fb232364ff3f2
Alex Wang <alexwang@Nicoles-MacBook.local>
2019-11-04 21:32:12 -0600

    Updated patrol--has bugs

--- Assets/Scripts/MonsterSystem/PatrolController.cs
+++ Assets/Scripts/MonsterSystem/PatrolController.cs
@@ -11,9 +11,16 @@ public class PatrolController : MonoBehaviour {
     private Rigidbody2D rb;
     public float patrolSpeed;
 
+    public Transform groundRay;
+    public float groundRayLength;
+    public LayerMask groundRayLayer;
+
+    private bool isGrounded;
     public Transform groundCheck;
-    public float groundCheckRayLength;
-    public LayerMask groundCheckLayer;
+    public float checkRadius;
+    public LayerMask groundLayer;
+
+    public float fallingSpeed;
 
     // Use this for initialization
     void Start () {
@@ -23,28 +30,34 @@ public class PatrolController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        RaycastHit2D isGrounded = Physics2D.Raycast(groundCheck.position, -groundCheck.up, groundCheckRayLength, patrolLayer);
-        Debug.DrawRay(groundCheck.position, -groundCheck.up * groundCheckRayLength, Color.red);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
 
-        if (isGrounded.collider != null) {
+        if (isGrounded) {
             Patrol();
         }
+        else
+        {
+            Vector3 pos = transform.position;
+            pos.y -= fallingSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        AdjustRotation();
         
     }
 
     private void Patrol()
     {
-        Vector2 pos = transform.position;
-        if (isMovingLeft)
-        {
-            pos.x += -patrolSpeed * Time.deltaTime;
+        Vector3 pos = transform.position;
+        if (isMovingLeft) {
+            pos -= patrolSpeed * Time.deltaTime * transform.right.normalized;
         }
         else
         {
-            pos.x += patrolSpeed * Time.deltaTime;
+            pos += patrolSpeed * Time.deltaTime * transform.right.normalized;
         }
         transform.position = pos;
-        RaycastHit2D hit = Physics2D.Raycast(patrolRay.position, -patrolRay.up, patrolRayLength, groundCheckLayer);
+        RaycastHit2D hit = Physics2D.Raycast(patrolRay.position, -patrolRay.up, patrolRayLength, patrolLayer);
         Debug.DrawRay(patrolRay.position, -patrolRay.up * patrolRayLength, Color.red);
 
         if (isMovingLeft && hit.collider == null)
@@ -68,5 +81,23 @@ public class PatrolController : MonoBehaviour {
         Vector3 scaler = transform.localScale;
         scaler.x *= -1;
         transform.localScale = scaler;
+    }
+
+    private void AdjustRotation()
+    {
+        RaycastHit2D groundHit = Physics2D.Raycast(groundRay.position, -groundRay.up, groundRayLength, groundRayLayer);
+        Debug.DrawRay(groundRay.position, -groundRay.up * groundRayLength, Color.red);
+        RaycastHit2D hitBottom = Physics2D.Raycast(groundRay.position, -groundRay.up, groundRayLength, groundRayLayer);
+        if (hitBottom.collider != null)
+        {
+            Vector3 surfaceNormal = hitBottom.normal; // Assign the normal of the surface to surfaceNormal
+            Vector3 forwardRelativeToSurfaceNormal = Vector3.Cross(transform.right, surfaceNormal);
+            Quaternion targetRotation = Quaternion.LookRotation(forwardRelativeToSurfaceNormal, surfaceNormal); //check For target Rotation.
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1f);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.identity, 1f);
+        }
     }
 }
